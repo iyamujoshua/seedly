3e1ae243238c4e7edb514701c32c479b9288bc7d
iyamujoshua <joshamu20@gmail.com>
2026-02-07 15:07:01 +0100

    feat: Move Firebase API keys to .env file for security

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:seedly/firebase_options.dart';
 import 'package:seedly/providers/auth_provider.dart';
@@ -8,6 +9,7 @@ import 'package:seedly/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
